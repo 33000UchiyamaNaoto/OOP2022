8c0daf378bb70f84cf1de9dbfffe7e2942098893
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2022-05-10 14:47:29 +0900

    問題2.1.1完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -4,22 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise {
+namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-            //2.1.3
-
-
-
-
-            //PrintSongs(songs);
-        }
-
-        //2.1.4
-        private static void PrintSongs(Song[] songs) {
-
-
-
         }
     }
 }
