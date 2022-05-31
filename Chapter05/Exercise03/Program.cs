e654dfbea3a4029f4a9b605cb557fba4bd886519
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2022-05-31 14:50:36 +0900

    第５章まで完成

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -8,7 +8,7 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
-
+            #region
             Exercise3_1(text);
             Console.WriteLine("-----");
 
@@ -20,26 +20,48 @@ namespace Exercise03 {
 
             Exercise3_4(text);
             Console.WriteLine("-----");
+
+            Exercise3_5(text);
+            #endregion
         }
 
         private static void Exercise3_1(string text) {
-
-
+            int spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
         }
 
         private static void Exercise3_2(string text) {
-
-
+            //P146
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-
-
+            //P148-P149
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}", count);
         }
 
         private static void Exercise3_4(string text) {
+            text.Split(' ').Where(word => word.Length <= 4)
+                                .ToList().ForEach(word => Console.WriteLine(word));
+            
+        }
 
+        private static void Exercise3_5(string text) {
+            var array = text.Split(' ').ToArray();
+            if(array.Length > 0)
+            {
+                var sb = new StringBuilder(array[0]);
+                foreach (var word in array.Skip(1))
+                {
+                    sb.Append(' ');
+                    sb.Append(word);                    
+                }
 
+                var str = sb.ToString();
+                Console.WriteLine(str);
+            }
         }
     }
 }
