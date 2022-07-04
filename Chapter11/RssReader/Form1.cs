3ee481c8a4454d5cc5ab2da8f491e86810d3fdce
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2022-07-04 09:40:54 +0900

    RssReader完成

---
+++ Chapter11/RssReader/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        IEnumerable<string> xTitle,xLink;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRssGet_Click(object sender, EventArgs e)
+        {
+            using (var wc = new WebClient())
+            {
+                var stream = wc.OpenRead(cbRssUrl.Text);
+
+                var xdoc = XDocument.Load(stream);
+                xTitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
+                xLink = xdoc.Root.Descendants("item").Select(x => (string)x.Element("link"));
+                foreach (var data in xTitle)
+                {
+                    lbRssTitle.Items.Add(data);
+                }
+            }
+        }
+
+        private void btBack_Click(object sender, EventArgs e)
+        {
+            wvBrowser.GoBack();
+        }
+
+        private void btForward_Click(object sender, EventArgs e)
+        {
+            wvBrowser.GoForward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackForwardButtonMaskCheck();
+        }
+
+        private void wvBrowser_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e)
+        {
+            BackForwardButtonMaskCheck();
+        }
+
+        private void BackForwardButtonMaskCheck()
+        {
+            //進む・戻るボタンのマスク設定
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btForward.Enabled = wvBrowser.CanGoForward;
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e)
+        {
+            int index = lbRssTitle.SelectedIndex;   //選択した箇所のインデックスを取得（０～　）
+            if (index == -1) return;
+            
+            var url = xLink.ElementAt(index); //xLink[index]と同じ
+            wvBrowser.Source = new Uri(url);        }
+    }
+}
