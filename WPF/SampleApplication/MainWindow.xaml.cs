07b6f54f3b65244cc5ce92138c20d5080da22488
Naoto Uchiyama <N-Uchiyama@ojs.ac.jp>
2022-10-20 11:18:24 +0900

    SampleSlider完成

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,14 +20,41 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = getSeasonIndex();
         }
 
-        private void okButton_Click(object sender, RoutedEventArgs e) {
+        private int getSeasonIndex() {
+            var today = DateTime.Now;
+                        
+            int ret = ((int)today.Month) / 3 - 1;
+            if (ret < 0)
+                ret = 3;
 
+            return ret;
         }
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
 
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
         }
     }
 }
